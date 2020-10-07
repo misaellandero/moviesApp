1f28ebe17ba0dce1a4953b9d733127fec8e4929a
misael-macAir <hola@landercorp.mx>
2020-10-07 13:24:23 -0500

    Initial Commit

---
+++ moviesApp/moviesAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  moviesAppApp.swift
+//  moviesApp
+//
+//  Created by Francisco Misael Landero Ychante on 07/10/20.
+//
+
+import SwiftUI
+
+@main
+struct moviesAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
